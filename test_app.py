886c2b97f2cdfd8c1327c737238badc8b30d71e9
RhythmAlam <almas.alam@ontariotechu.net>
2025-11-17 22:42:30 -0500

    Move workflows to repo root and add CI files

--- test_app.py
+++ test_app.py
@@ -1,10 +1,12 @@
 import app
 
+
 def test_hello():
     client = app.app.test_client()
     response = client.get('/hello')
     assert response.status_code == 200
     assert response.get_json() == {"message": "Hello, World!"}
+
 
 def test_echo():
     client = app.app.test_client()
